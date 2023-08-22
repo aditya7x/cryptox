ccc5f109f1fb1b9643aaf5872cbda31e6346f1b7
aditya7x <142424456+aditya7x@users.noreply.github.com>
2023-08-22 21:15:21 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -18,7 +18,25 @@ app.use(
     
   })
 );
-app.options('*', cors()) // include before other routes
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://cryptox-6149.vercel.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 app.use(express.static(__dirname + '/public/'))
