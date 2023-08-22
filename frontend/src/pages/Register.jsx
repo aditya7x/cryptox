b6927d1b6fd40168ecc6f1daf65649b5367783de
aditya7x <142424456+aditya7x@users.noreply.github.com>
2023-08-22 19:52:05 +0530

    Update xRegister.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -31,6 +31,10 @@ const handleOnSubmit = async (e) => {
   
     let result = await fetch(
     'https://cryptox-nine.vercel.app/register', {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        }
     
         method: "post",
         body: JSON.stringify({firstname, lastname, email, password ,funds}),
