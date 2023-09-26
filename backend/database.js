6be7bf710f6a9984c24dc11614f4cbb8462a421d
aditya7x <142424456+aditya7x@users.noreply.github.com>
2023-09-26 22:30:07 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -11,12 +11,12 @@ mongoose.connect(DB);
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.json());
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// })
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 
 // app.all('/', function(req, res, next) {
